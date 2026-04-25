3198094245c3f3cb958255bfe694f55759f6ff7a
grumpyGrampsDev <271845063+82fnszt9h9-code@users.noreply.github.com>
2026-04-25 19:48:33 -0400

    add computer choice funtion and minimal css

--- src/script.js
+++ src/script.js
@@ -1,5 +1,14 @@
-const foo = "bar";
-function test() {
-  console.log(foo);
+console.log("The script is loaded");
+
+function getComputerChoice(string) {
+  let choice = Math.floor(Math.random() * 10);
+  if (choice <= 3) {
+    console.log("Rock");
+  } else if (choice > 3 && choice < 7) {
+    console.log("Paper");
+  } else if (choice >= 7 && choice <= 10) {
+    console.log("Scissors");
+  } else {
+    console.log("A value outside the expected range was returned");
+  }
 }
-const unused = 123;
